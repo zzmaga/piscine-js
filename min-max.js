2637acd1ef715e6e1a8551ee03ff8f6851eb3497
mtastan <magjantastanov@gmail.com>
2025-07-06 02:12:13 +0500

    min-max

---
+++ min-max.js
@@ -0,0 +1,14 @@
+/*
+Create a function named max that takes 2 number arguments and returns the largest of the two.
+You must not just use Math.max, make your own.
+Create a function named min which is the same as max, but returns the lowest.
+You must not just use Math.min, make your own.
+*/
+
+function max(a, b) {
+  return a > b ? a : b;
+}
+
+function min(a, b) {
+  return a < b ? a : b;
+}
